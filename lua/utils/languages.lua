917754d8b3b6d7345aa47c7062a0dd85abaec407
peturparkur <peter@nagymathe.xyz>
2025-08-28 20:55:15 +0200

    cpp with clangd

--- lua/utils/languages.lua
+++ lua/utils/languages.lua
@@ -8,6 +8,9 @@
 --  - settings (table): Override the default settings passed when initializing the server.
 --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 local Languages = {
+  ['cpp'] = {
+    clangd = {},
+  },
   ['python'] = {
     -- pyrefly = {},
     pyright = {
