c8900193ac6be424aaf22f828d3aaf729ad6eef6
peturparkur <peter@nagymathe.xyz>
2026-01-03 23:12:13 +0100

    add formatters + dap to different language setups

--- lua/custom/languages/cpp.lua
+++ lua/custom/languages/cpp.lua
@@ -1,5 +1,8 @@
+-- local codelldb = require "mason-nvim-dap.mappings.adapters.codelldb"
 return {
   lsp = {
     clangd = {},
   },
+  dap = { codelldb = {} },
+  format = { ['clang-format'] = {} },
 }
