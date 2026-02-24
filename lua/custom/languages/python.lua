67c9985cd6c7261e131ed86c36dca5e595b4fc78
peturparkur <61064454+peturparkur@users.noreply.github.com>
2026-02-24 21:52:32 +0100

    add formatters + dap to different language setups (#11)
    * add formatters + dap to different language setups
    
    * rust setup
    
    * add copilot to home neovim setup
    
    * remove some prints

--- lua/custom/languages/python.lua
+++ lua/custom/languages/python.lua
@@ -1,40 +1,62 @@
 return {
-  dap = { python = {} },
+  dap = {
+    -- <name of DAP> -> configuration
+    python = {
+      {
+        name = 'Python: Launch file',
+        type = 'python',
+        request = 'launch',
+        program = '${file}',
+        console = 'integratedTerminal',
+      },
+      {
+        name = 'Pytest: Current File',
+        type = 'python',
+        request = 'launch',
+        module = 'pytest',
+        args = {
+          '${file}',
+          '-sv',
+        },
+        console = 'integratedTerminal',
+      },
+    },
+  },
   lsp = {
-    -- pyrefly = {},
-    pyright = {
-      settings = {
-        python = {
-          analysis = {
-            autoSearchPaths = true,
-            diagnosticMode = 'workspace',
-            useLibraryCodeForTypes = true,
-            autoImportCompletions = true,
-          },
-        },
-      },
-      disableLanguageServices = false,
-    },
-    basedpyright = {
-      capabilities = {
-        -- Basedpyright does not support these capabilities well.
-        definitionProvider = false,
-        typeDefinitionProvider = false,
-        implementationProvider = false,
-        referencesProvider = false,
-        -- hoverProvider = false, -- decide if pyright or basedpyright
-      },
-      settings = {
-        basedpyright = {
-          analysis = {
-            autoSearchPaths = true,
-            typeCheckingMode = 'basic',
-            diagnosticMode = 'openFilesOnly',
-            useLibraryCodeForTypes = true,
-          },
-        },
-      },
-    },
+    pyrefly = {},
+    -- pyright = {
+    --   settings = {
+    --     python = {
+    --       analysis = {
+    --         autoSearchPaths = true,
+    --         diagnosticMode = 'workspace',
+    --         useLibraryCodeForTypes = true,
+    --         autoImportCompletions = true,
+    --       },
+    --     },
+    --   },
+    --   disableLanguageServices = false,
+    -- },
+    -- basedpyright = {
+    --   capabilities = {
+    --     -- Basedpyright does not support these capabilities well.
+    --     definitionProvider = false,
+    --     typeDefinitionProvider = false,
+    --     implementationProvider = false,
+    --     referencesProvider = false,
+    --     -- hoverProvider = false, -- decide if pyright or basedpyright
+    --   },
+    --   settings = {
+    --     basedpyright = {
+    --       analysis = {
+    --         autoSearchPaths = true,
+    --         typeCheckingMode = 'basic',
+    --         diagnosticMode = 'openFilesOnly',
+    --         useLibraryCodeForTypes = true,
+    --       },
+    --     },
+    --   },
+    -- },
   },
   format = {
     ruff = {},
