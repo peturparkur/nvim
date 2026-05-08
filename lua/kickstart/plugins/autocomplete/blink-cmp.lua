aa2141fe412b426d9f100817704391c2c9bc35ca
peturparkur <peter@nagymathe.xyz>
2026-05-08 10:42:30 +0200

    some minor adjustments to blink completion

--- lua/kickstart/plugins/autocomplete/blink-cmp.lua
+++ lua/kickstart/plugins/autocomplete/blink-cmp.lua
@@ -72,7 +72,10 @@ return { -- Autocompletion
     completion = {
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      trigger = {
+        show_on_insert = true,
+      },
     },
 
     sources = {
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return { -- Highlight, edit, and navigate code
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
     },
-    -- indent = { enable = true, disable = { 'ruby' } },
+    indent = { enable = true, disable = { 'ruby' } },
   },
   config = function(_, opts)
     -- fold method to be function / treesitter
