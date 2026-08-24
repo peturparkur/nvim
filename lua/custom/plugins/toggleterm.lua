66300cbd33e81196f7be1d912c8c3c2708e76d22
peturparkur <peter@nagymathe.xyz>
2026-08-24 10:56:38 +0200

    have new version of toggleterm to have TermNew command

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,6 +1,6 @@
 return {
   'akinsho/toggleterm.nvim',
-  version = '*',
+  version = '9a88eae817ef395952e08650b3283726786fb5fb', -- want commit version since latest release does not contain TermNew command
   opts = {},
   config = function(_, opts)
     require('toggleterm').setup {
